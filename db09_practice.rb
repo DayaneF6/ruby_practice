c84313e2ae9f09057bd02765f862bb1afac2c44f
DayaneF6 <dayanef652@gmail.com>
2023-07-21 19:46:38 -0300

    update exercises ruby and mysql

--- db09_practice.rb
+++ db09_practice.rb
@@ -2,12 +2,14 @@ require 'mysql2'
 require 'dotenv/load'
 require_relative 'methods.rb'
 
-
 client = Mysql2::Client.new(host: "db09.blockshopper.com", username: ENV['DB09_LGN'], password: ENV['DB09_PWD'], database: "applicant_tests")
-
+=begin
 get_teacher(3, client)
 get_subject_teachers(1, client)
 get_class_subjects('Math', client)
 get_teachers_list_by_letter('Joao', 'Barbosa', client)
+set_md5(client)
+=end
+get_class_info(3, client)
 
 client.close
--- methods.rb
+++ methods.rb
@@ -1,11 +1,18 @@
+require 'digest'
+
 
 def get_teacher(id, client)
-  f = "select first_name, middle_name, last_name, birth_date from teachers_dayane where ID = #{id}"
+  f = "select first_name, middle_name, last_name, birth_date, s.name from teachers_dayane t JOIN subjects_dayane s ON t.id = s.id WHERE t.id = #{id}"
   results = client.query(f).to_a
   if results.count.zero?
     puts "Teacher with ID #{id} was not found."
   else
-    puts "Teacher #{results[0]['first_name']} #{results[0]['middle_name']} #{results[0]['last_name']} was born on #{(results[0]['birth_date']).strftime("%d %b %Y (%A)")}"
+    string = "Subject: #{results[0]['name']}\nTeachers:"
+    results.each do |row|
+      string += "#{row['first_name']} #{row['middle_name']} #{row['last_name']}\n"
+    end
+    puts string
+    #puts "Teacher #{results[0]['first_name']} #{results[0]['middle_name']} #{results[0]['last_name']} was born on #{(results[0]['birth_date']).strftime("%d %b %Y (%A)")}"
   end
 end
 
@@ -17,33 +24,14 @@ def get_subject_teachers(id, client)
   if results.empty?
     p "Subject with ID #{id} was not found."
   else
-    p "Subject #{results[0]['name']} - Teacher #{results[0]['first_name']} #{results[0]['middle_name']} #{results[0]['last_name']}"
+    string = "Subject: #{results[0]['name']}\nTeachers:"
+    results.each do |row|
+      string += "#{row['first_name']} #{row['middle_name']} #{row['last_name']}\n"
+    end
+    puts string
+    #p "Subject #{results[0]['name']} - Teacher #{results[0]['first_name']} #{results[0]['middle_name']} #{results[0]['last_name']}"
   end
 end
-
-=begin def get_subject_teachers(id, client)
-  # Get subject name
-  subject_query = "SELECT name FROM subjects_dayane WHERE ID = #{id}"
-  subject_result = client.query(subject_query).to_a
-  if subject_result.empty?
-    p "Subject with ID #{id} was not found."
-    return
-  end
-  name = subject_result[0]['name']
-  # Get teachers for the subject
-  teachers_query = "SELECT t.first_name, t.middle_name, t.last_name FROM teachers_dayane t JOIN subjects_dayane st ON t.ID = st.id WHERE st.id = #{id}"
-  teachers_result = client.query(teachers_query).to_a
-  if teachers_result.empty?
-    p "No teachers found for subject with ID #{id}."
-    return
-  end
-  p "Subject: #{name}"
-  p "Teachers:"
-  teachers_result.each do |teacher|
-    p "#{teacher['first_name']} #{teacher['middle_name']} #{teacher['last_name']}"
-  end
-end
-=end
 
 def get_class_subjects(name, client)
     c = "SELECT c.name, td.first_name, td.middle_name, td.last_name FROM classes_dayane c
@@ -53,7 +41,12 @@ def get_class_subjects(name, client)
     if results.empty?
       p "Class with name #{name} was not found."
     else
-      p "Class #{results[0]['name']} - Teacher #{results[0]['first_name']} #{results[0]['middle_name']} #{results[0]['last_name']}"
+      string = "Class: #{results[0]['name']}\nTeachers:"
+      results.each do |row|
+        string += "#{row['first_name']} #{row['middle_name']} #{row['last_name']}\n"
+      end
+      puts string
+      #p "Class #{results[0]['name']} - Teacher #{results[0]['first_name']} #{results[0]['middle_name']} #{results[0]['last_name']}"
     end
 end
 
@@ -67,7 +60,49 @@ def get_teachers_list_by_letter(letter1, letter2, client)
   if results.empty?
     p "Teacher with name #{letter1} #{letter1}  was not found."
   else
-    p "Teacher #{results[0]['first_name']} #{results[0]['middle_name']} #{results[0]['last_name']} - Subject #{results[0]['name']} - "
+    string = "Subject: #{results[0]['name']}\nTeachers:"
+    results.each do |row|
+      string += "#{row['first_name']} #{row['middle_name']} #{row['last_name']}\n"
+    end
+    puts string
+    #p "Teacher #{results[0]['first_name']} #{results[0]['middle_name']} #{results[0]['last_name']} - Subject #{results[0]['name']} - "
   end
 end
+
+
+def set_md5(client)
+  t = "SELECT * FROM teachers_dayane"
+  teachers = client.query(t).to_a
+
+  teachers.each do |teacher|
+    hash_t = Digest::MD5.hexdigest "#{teacher[:first_name]}#{teacher[:middle_name]}#{teacher[:last_name]}#{teacher[:birth_date]}#{teacher[:subject_id]}#{teacher[:current_age]}"
+    update_qry = "UPDATE teachers_dayane SET md5 = \"#{hash_t}\" WHERE id = #{teacher['id']};"
+    client.query(update_qry)
+  end
+end
+
+
+
+#not finished
+
+def get_class_info(id, client)
+  c = "SELECT t.first_name, t.middle_name, t.last_name, c.name FROM teachers_dayane t
+      JOIN teachers_classes_dayane tc ON t.subject_id = tc.teacher_id
+      JOIN classes_dayane c ON tc.class_id = c.id WHERE tc.id = \"#{id}\""
+  results = client.query(c).to_a
+
+  if results.empty?
+    p "Class with #{id} was not found."
+  else
+    string = "Class name: #{results[0]['name']}\nTeachers:"
+    results.each do |row|
+      string += "#{row['first_name']} #{row['middle_name']} #{row['last_name']}\n"
+    end
+    puts string
+
+  end
+
+end
+
+
 
