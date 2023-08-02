6430a531fd3020cd558fdeebff94233961c8866b
DayaneF6 <dayanef652@gmail.com>
2023-08-02 15:16:27 -0300

    update exercises

--- db09_practice.rb
+++ db09_practice.rb
@@ -1,8 +1,10 @@
 require 'mysql2'
 require 'dotenv/load'
 require_relative 'methods.rb'
+# require_relative 'add_data.rb'
 
 client = Mysql2::Client.new(host: "db09.blockshopper.com", username: ENV['DB09_LGN'], password: ENV['DB09_PWD'], database: "applicant_tests")
+
 =begin
 get_teacher(3, client)
 get_subject_teachers(1, client)
@@ -10,10 +12,9 @@ get_class_subjects('physics', client)
 get_teachers_list_by_letter('Barbosa', client)
 set_md5(client)
 get_class_info(3, client)
+get_teachers_by_year(1997, client)
 =end
 
-get_teachers_by_year(1997, client)
-
-
+generate_random_people(client, 20)
 
 client.close
--- methods.rb
+++ methods.rb
@@ -22,14 +22,14 @@ def get_subject_teachers(id, client)
   results = client.query(c).to_a
 
   if results.empty?
-    p "Subject with ID #{id} was not found."
-  else
-    string = "Subject: #{results[0]['name']}\nTeachers:\n"
-    results.each do |row|
-      string += "#{row['first_name']} #{row['middle_name']} #{row['last_name']}\n"
-    end
-    puts string
-    #p "Subject #{results[0]['name']} - Teacher #{results[0]['first_name']} #{results[0]['middle_name']} #{results[0]['last_name']}"
+      p "Subject with ID #{id} was not found."
+    else
+      string = "Subject: #{results[0]['name']}\nTeachers:\n"
+      results.each do |row|
+        string += "#{row['first_name']} #{row['middle_name']} #{row['last_name']}\n"
+      end
+      puts string
+      #p "Subject #{results[0]['name']} - Teacher #{results[0]['first_name']} #{results[0]['middle_name']} #{results[0]['last_name']}"
   end
 end
 
@@ -48,14 +48,14 @@ def get_class_subjects(name, client)
     results = client.query(c).to_a
 
     if results.empty?
-      p "Class with name #{name} was not found."
-    else
-      string = "Class: #{results[0]['class']}\nSubject: #{results[0]['subject']}\nTeachers:\n"
-      results.each do |row|
-        string += "#{row['first_name']} #{row['middle_name']} #{row['last_name']}\n"
-      end
-      puts string
-      #p "Class #{results[0]['name']} - Teacher #{results[0]['first_name']} #{results[0]['middle_name']} #{results[0]['last_name']}"
+        p "Class with name #{name} was not found."
+      else
+        string = "Class: #{results[0]['class']}\nSubject: #{results[0]['subject']}\nTeachers:\n"
+        results.each do |row|
+          string += "#{row['first_name']} #{row['middle_name']} #{row['last_name']}\n"
+        end
+        puts string
+        #p "Class #{results[0]['name']} - Teacher #{results[0]['first_name']} #{results[0]['middle_name']} #{results[0]['last_name']}"
     end
 end
 
@@ -64,18 +64,18 @@ def get_teachers_list_by_letter(letter, client)
         JOIN subjects_dayane s ON t.subject_id = s.id WHERE t.first_name
         LIKE '%#{letter}%' OR t.last_name LIKE '%#{letter}%'"
 
-  results = client.query(tl).to_a
+    results = client.query(tl).to_a
 
-  if results.empty?
-    p "Teacher with name #{letter} was not found."
-  else
-    string = "Subject: #{results[0]['name']}\nTeachers:"
-    results.each do |row|
-      string += "#{row['first_name']} #{row['middle_name']} #{row['last_name']}\n"
+    if results.empty?
+        p "Teacher with name #{letter} was not found."
+      else
+        string = "Subject: #{results[0]['name']}\nTeachers:"
+        results.each do |row|
+          string += "#{row['first_name']} #{row['middle_name']} #{row['last_name']}\n"
+        end
+        puts string
+        #p "Teacher #{results[0]['first_name']} #{results[0]['middle_name']} #{results[0]['last_name']} - Subject #{results[0]['name']} - "
     end
-    puts string
-    #p "Teacher #{results[0]['first_name']} #{results[0]['middle_name']} #{results[0]['last_name']} - Subject #{results[0]['name']} - "
-  end
 end
 
 
@@ -90,72 +90,122 @@ def set_md5(client)
   end
 end
 
-
-=begin def get_class_info(id, client)
-  c = "SELECT t.first_name, t.middle_name, t.last_name, c.name FROM teachers_dayane t
-      JOIN teachers_classes_dayane tc ON t.subject_id = tc.teacher_id
-      JOIN classes_dayane c ON tc.class_id = c.id WHERE tc.class_id = \"#{id}\";"
-  results = client.query(c).to_a
-
-  if results.empty?
-    p "Class with #{id} was not found."
-  else
-    string = "Class name: #{results[0]['name']}\nResponsible Teacher:"
-    results.each do |row|
-      string += "#{row['first_name']} #{row['middle_name']} #{row['last_name']}\n"
-    end
-    puts string
-
-  end
-end
-=end
 def get_class_info(class_id, client)
-  involved_teachers = "SELECT c.name, t.first_name, t.last_name
-   FROM teachers_classes_dayane AS tc
-     JOIN classes_dayane AS c
-     ON tc.class_id = c.id
-     JOIN teachers_dayane AS t
-     ON tc.teacher_id = t.id
-     WHERE c.id = #{class_id};"
+    involved_teachers = "SELECT c.name, t.first_name, t.last_name
+       FROM teachers_classes_dayane AS tc
+         JOIN classes_dayane AS c
+         ON tc.class_id = c.id
+         JOIN teachers_dayane AS t
+         ON tc.teacher_id = t.id
+         WHERE c.id = #{class_id};"
 
-  responsible_teacher = "SELECT c.name, t.first_name, t.last_name
-   FROM classes_dayane AS c
-     JOIN teachers_dayane AS t
-       ON c.responsible_teacher_id = t.id
-   WHERE c.id = #{class_id};"
+      responsible_teacher = "SELECT c.name, t.first_name, t.last_name
+         FROM classes_dayane AS c
+           JOIN teachers_dayane AS t
+             ON c.responsible_teacher_id = t.id
+         WHERE c.id = #{class_id};"
 
-  results_involved = client.query(involved_teachers).to_a
-  results_responsible = client.query(responsible_teacher).to_a
+    results_involved = client.query(involved_teachers).to_a
+    results_responsible = client.query(responsible_teacher).to_a
 
-  if results_involved.empty? || results_responsible.empty?
-    puts "There are no involved or responsible teachers in class with id #{class_id}"
-  else
-    string = "Class name: #{results_responsible[0]['name']}\nResponsible teacher: #{results_responsible[0]['first_name']} #{results_responsible[0]['last_name']}\nInvolved teachers:\n"
+    if results_involved.empty? || results_responsible.empty?
+        puts "There are no involved or responsible teachers in class with id #{class_id}"
+      else
+        string = "Class name: #{results_responsible[0]['name']}\nResponsible teacher: #{results_responsible[0]['first_name']} #{results_responsible[0]['last_name']}\nInvolved teachers:\n"
 
-    results_involved.each do |row|
-      string += "#{row['first_name']} #{row['last_name']}\n"
+        results_involved.each do |row|
+          string += "#{row['first_name']} #{row['last_name']}\n"
+        end
+
+        puts string.strip
     end
-
-    puts string.strip
-  end
 end
 
 def get_teachers_by_year(year, client)
-  year_t = "SELECT t.first_name, t.last_name FROM teachers_dayane t WHERE YEAR(birth_date) = \"#{year}\";"
-  results = client.query(year_t).to_a
+    year_t = "SELECT t.first_name, t.last_name FROM teachers_dayane t WHERE YEAR(birth_date) = \"#{year}\";"
+    results = client.query(year_t).to_a
 
-  if results.empty?
-    p "There are no teachers born in #{year}"
-  else
-    string = ""
+    if results.empty?
+        p "There are no teachers born in #{year}"
+      else
+        string = ""
 
-    results.each do |row|
-      string += "Teachers born in #{year}: #{row['first_name']} #{row['last_name']}"
+        results.each do |row|
+          string += "Teachers born in #{year}: #{row['first_name']} #{row['last_name']}"
+
+        end
+        p string
+    end
+end
+
+#--------------------------------------------------------------------------------------------------------------------------------------
+#exercicio para gerar registros na tabela
+# não finalizado ainda
+
+def random_date(st_date, end_date)
+  st_date = Date.parse('1923-01-01')
+  end_date = Date.parse('2023-08-01')
+  random_date = rand(st_date..end_date)
+  random_date.strftime('%Y-%m-%d')
+end
+
+def random_last_names(n, client)
+  query = <<~SQL
+      SELECT last_name
+      FROM teachers_dayane
+      ORDER BY RAND()
+      LIMIT 1
+  SQL
+  result = client.query(query)
+  # result['last_name']
+  result.map { |row| row['last_name'] }
+end
+
+def random_first_names(n, client)
+  query = <<~SQL
+      SELECT first_name
+      FROM teachers_dayane
+      ORDER BY RAND()
+      LIMIT 1
+  SQL
+  result = client.query(query)
+  # result['first_name']
+  result.map { |row| row['first_name'] }
+end
+
+def generate_random_people(client, n)
+
+  first_names = random_first_names(n, client)
+  last_names = random_last_names(n, client)
+  birth_dates = []
+
+  n.times do
+    birth_dates << random_date("1923-01-01","2023-01-01")
+  end
+
+  # data is an array of arrays with first_name, last_name and birth_date keys
+  data = first_names.zip(last_names).zip(birth_dates).map(&:flatten)
+
+  data.each_slice(20000) do |group|
+    insert = "INSERT INTO random_people_dayane (first_name, last_name, birth_date) VALUES "
+
+    # row is ["Name1", "Lastname", "2001-06-01"]
+    group.each do |row|
+      insert += "(\"#{row[0]}\", \"#{row[1]}\", \"#{row[2]}\"),"
 
     end
-    p string
+    client.query(insert.chop!)
   end
 end
+
+# def random_people(client, n)
+#   if n <= 20000
+#     generate_random_people(client, n)
+#     else
+#       random_people(20000, client)
+#       random_people(n - 20000, client)
+#   end
+# end
 
 
 
